2a5bc2b45461bcdabed8c11e9b629927ed3e5832
yudistiraadiyoga <117887589+yudistiraadiyoga@users.noreply.github.com>
2023-03-01 19:07:25 +0700

    Add files via upload

---
+++ 4_D_71220836.py
@@ -0,0 +1,5 @@
+b = str(input('Masukkan sekumpulan bilangan (pisahkan dengan koma)\t: ')).split(',')
+terbesar = lambda b: b.split()
+terkecil = lambda b: b.split()
+print(f'Bilangan terbesar dari kumpulan bilangan yang di input adalah {terbesar(b)}')
+print(f'Bilangan terkecil dari kumpulan bilangan yang di input adalah {terkecil(b)}')
